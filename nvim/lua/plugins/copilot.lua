cd18388ce89d8802273dbe658388a8dfcc1ed91e
himuraxkenji <aaron.ariperto@gmail.com>
2025-04-11 22:00:52 -0300

    change copilot configuration

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,17 +1,24 @@
 return {
+
   "zbirenbaum/copilot.lua",
   opts = {
-    filetypes = {
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
+    panel = {
+      auto_refresh = false,
+      keymap = {
+        accept = "<CR>",
+        jump_prev = "[[",
+        jump_next = "]]",
+        refresh = "gr",
+        open = "<M-CR>",
+      },
+    },
+    suggestion = {
+      auto_trigger = true,
+      keymap = {
+        accept = "<C-CR>",
+        prev = "<M-[>",
+        next = "<M-]>",
+        dismiss = "<C-]>",
       },
     },
   },
