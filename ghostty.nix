68e22e318dae3b20eca89f43ba5e4d6dfe930c12
himuraxkenji <aaron.ariperto@gmail.com>
2025-04-27 19:59:42 -0300

    add catppuccin theme, and avante support with sonnet

--- ghostty.nix
+++ ghostty.nix
@@ -2,15 +2,13 @@
 {
   home.activation.copyGhostty = lib.hm.dag.entryAfter ["writeBoundary"] ''
     echo "Copying Ghostty configuration..."
-    rm -rf "$HOME/.config/ghostty"
+    rm -rf "$HOME/.config/ghostty"/*
 
-    # Check if the .config directory exists, if not, create it
-    # if [ ! -d "$HOME/.config/ghostty" ]; then
-    #   mkdir -p "$HOME/.config/ghostty"
-    # fi
+    if [ ! -d "$HOME/.config/ghostty" ]; then
+      mkdir -p "$HOME/.config/ghostty"
+    fi
 
-    cp -r ${toString ./ghostty} "$HOME/.config/ghostty"
+    cp -r ${toString ./ghostty}/* "$HOME/.config/ghostty/"
     chmod -R u+w "$HOME/.config/ghostty"
   '';
 }
-
